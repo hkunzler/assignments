2d12f83a65e550f80d6ec9cab0b20c7277d6455c
Harmony Kunzler <hkunzler@gmail.com>
2019-01-24 17:53:13 -0700

    grocery-cart, hello-world-jokes, debugging

---
+++ exercises/grocery-store/app.js
@@ -0,0 +1,11 @@
+var shopper = {
+    name: "Arya",
+    age: 9,
+    hungry: 10 > 9,
+    itemsForgot: function() {
+        return ("Not the chocolate!")
+    },
+    groceryCart: ["milk", "eggs", "butter"]
+}
+
+console.log(shopper.hungry);
---
+++ exercises/hello-class/app.js
@@ -0,0 +1,4 @@
+var sayHello = function(name, age){
+    console.log("hello " + name + ", how's being " + age + "?");
+}
+sayHello("Nicholas", 23)
