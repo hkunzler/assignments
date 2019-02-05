5d88bef0e23fa758eb1bcfa9cb3a491a355b8a49
Harmony Kunzler <hkunzler@gmail.com>
2019-02-04 17:15:18 -0700

    mario touch up

--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -1,20 +1,20 @@
 var mario = document.mario
 var marioContainer = document.getElementById('marioContainer')
 
-mario.addEventListener("submit", function(e){
+mario.addEventListener("submit", function (e) {
     e.preventDefault()
     marioContainer.innerHTML = ''
-    if(isNaN(parseInt(mario.goombas.value)) === true){
-        mario.goombas.value = 0;
-    } 
-    if(isNaN(parseInt(mario.bobombs.value)) === true){
+    if (isNaN(parseInt(mario.goombas.value)) === true) {
+        marioÎ.goombas.value = 0;
+    }
+    if (isNaN(parseInt(mario.bobombs.value)) === true) {
         mario.bobombs.value = 0;
     }
-    if(isNaN(parseInt(mario.cheepcheeps.value)) === true){
+    if (isNaN(parseInt(mario.cheepcheeps.value)) === true) {
         mario.cheepcheeps.value = 0;
     }
     var total = parseInt(mario.goombas.value) * 5 + parseInt(mario.bobombs.value) * 7 + parseInt(mario.cheepcheeps.value) * 11
-    var totalBaddies = parseInt(mario.goombas.value) + parseInt(mario.bobombs.value)  + parseInt(mario.cheepcheeps.value)
+    var totalBaddies = parseInt(mario.goombas.value) + parseInt(mario.bobombs.value) + parseInt(mario.cheepcheeps.value)
     mario.goombas.value = ''
     mario.bobombs.value = ''
     mario.cheepcheeps.value = ''
